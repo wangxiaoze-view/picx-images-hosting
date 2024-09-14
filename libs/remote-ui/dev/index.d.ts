61df0e90219eb06fb512ad1a276c1668a18b86d4
王小泽 <wangxiaoze@petalmail.com>
2024-09-14 15:04:22 +0800

    fix: 回滚插图，删除冗余文件

--- libs/remote-ui/dev/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { App } from 'vue';
-import { default as MyComponent } from './component-a/index.vue';
-
-declare const install: (app: App<Element>) => void;
-export { MyComponent, install };
-declare const _default: {
-    install: (app: App<Element>) => void;
-};
-export default _default;
--- libs/remote-ui/dev/utils/time.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function getTime(format?: string): string;
