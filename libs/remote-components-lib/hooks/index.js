dc62c3110cc0f224a00b3576d90e0a9b395a4ff1
王小泽 <wangxiaoze@petalmail.com>
2024-09-14 15:08:11 +0800

    fix: 回滚插图，删除冗余文件

--- libs/remote-components-lib/hooks/index.js
+++
@@ -1,8 +0,0 @@
-import { F as a, D as u, u as o, H as m, G as p } from "../element/AmountText/index-BGZt47l8.js";
-export {
-  a as useComputedNum,
-  u as useEleForm,
-  o as useLoading,
-  m as useNavigatorApp,
-  p as useResize
-};
